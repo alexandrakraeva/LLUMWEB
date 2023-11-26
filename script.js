a6d6c48260a262c7cecdc1ee680dd5166e073c03
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2023-11-26 13:59:15 +0100

    add files

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿document.getElementById('playButton').addEventListener('click', function () {
+    this.style.display = 'none';  // Hide the button
+    var video = document.getElementById('myVideo');
+    video.style.display = 'block';  // Show the video
+    video.play();  // Play the video
+});
